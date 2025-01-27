38e7c664f048113db02ccd4879f8a464428c773b
Robby Clifford <jaspercollign10@gmail.com>
2025-01-27 20:09:57 +0800

    self, cls, non-static

--- PRG/2024/Python/Main/.venv/tests.py
+++ PRG/2024/Python/Main/.venv/tests.py
@@ -1,24 +1,26 @@
+class person:
+    access_this = 266 #THIS IS A STATIC VARIABLE/ATTRIBUTE! IT IS SHARED AMONG ALL INSTANCES WTF
+    
+    def if_existing(self, value):
+        self.value = 255
+        return self.value
+    
+    @classmethod
+    def if_not(cls):
+        print(cls.access_this)
+        
+class book:
+    def __init__(self, value):#FOR CONSTRUCTOR AND NON-STATIC OR SHARED CLASS ATTRIBUTES
+        self.value = value
+    
+obj1 = person
+obj1.access_this = 2
+print(obj1.access_this)
 
+obj2 = person
+print(obj2.access_this)
 
-history = ([], set({}))
-amount = 0
-while True:
-    wallet = input(f"Ammount: {amount:02} : (f2/add)(f1/decrease)(f5/history)(f4/exit): ")
-    match wallet:
-        case 'f2':
-            history[0].append(input("Enter ammount or press f3 to cancel: "))
-            temp = history[0].pop()
-            if temp == "f3":
-                continue
-            history[0].append(int(temp))
-            amount += int(temp)
-        case 'f1':
-            temp = input("Input ammount to decrease or press f3 to cancel: ")
-            if temp == "f3":
-                continue
-            amount -= int(temp)
-            history[1].add(int(temp))
-        case 'f5':
-            print(f"history : {history}")
-        case 'f4':
-            break
+obj3 = book(255)
+print(obj3.value)
+
+obj4 = book(265)
